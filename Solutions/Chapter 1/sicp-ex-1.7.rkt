6f2b54d68cc82b50b89751603ce8e479f652f6ec
xsw0 <xsw.mail.0@gmail.com>
2020-06-17 15:19:23 +0800

    Create sicp-ex-1.7.rkt

---
+++ Solutions/Chapter 1/sicp-ex-1.7.rkt
@@ -0,0 +1,29 @@
+#lang sicp
+(define (square x) (* x x))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (good-enough? guess next-guess)
+  (< (/ (abs (- next-guess guess)) guess) 0.1))
+
+(define (square-root x)
+  (sqrt-iter x 1.0 (improve 1.0 x)))
+
+(define (sqrt-iter x guess next-guess)
+  (if (good-enough? guess next-guess)
+      guess
+      (sqrt-iter x
+                 next-guess
+                 (improve next-guess x))))
+
+(square-root 9)
+(square-root 0.0001)
+(square-root 10000000000000.0001)
+(square-root 100000000000000000000)
+(square-root 100000000000000000000000000)
+(square-root 0.0000000000001)
+;(square-root 0)
