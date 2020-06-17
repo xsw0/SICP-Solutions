03bc1fa42e5f839a6fdebf996160eb6894d92311
xsw0 <xsw.mail.0@gmail.com>
2020-06-17 14:13:14 +0800

    1.3 1.5 1.5

--- Solutions/Chapter 1/sicp-ex-1.3.rkt
+++ Solutions/Chapter 1/sicp-ex-1.3.rkt
@@ -1,18 +1,16 @@
 #lang sicp
 (define (square x) (* x x))
 
+(define (sum-of-squares x y)
+  (+ (square x) (square y)))
+
 (define (sum-of-squares-larger n1 n2 n3)
-  (cond ((and (<= n1 n2)
-              (<= n1 n3))
-         (+ (square n2)
-            (square n3)))
-        ((and (<= n2 n1)
-              (<= n2 n3))
-         (+ (square n1)
-            (square n3)))
+  (cond ((and (<= n1 n2) (<= n1 n3))
+         (sum-of-squares n2 n3))
+        ((and (<= n2 n1) (<= n2 n3))
+         (sum-of-squares n1 n3))
         (else
-         (+ (square n1)
-            (square n2)))))
+         (sum-of-squares n1 n2))))
 
 
 (sum-of-squares-larger 1 2 3)   ;Value: 13 
